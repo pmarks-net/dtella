742484e39b1bc597d16b9e434af4c089187dd50c
Paul Marks <paul@pmarks.net>
2008-08-10 02:33:07 +0000

    - Merge in the fancy OSX installer from trunk.

--- build_osx.sh
+++ build_osx.sh
@@ -5,13 +5,27 @@ else
   FILE_BASE="dtella-$1"
 fi
 
+rm -r build
 rm -r dist
+rm installer_osx/template.sparseimage
 rm $FILE_BASE.dmg
 
-mkdir dist
-cp docs/readme.txt dist/
-cp docs/changelog.txt dist/
-cp docs/gpl.txt dist/
+python setup.py py2app || exit
 
-python setup.py py2app
-hdiutil create -srcfolder dist -volname $FILE_BASE $FILE_BASE.dmg
+hdiutil eject /Volumes/Dtella
+hdiutil eject /Volumes/$FILE_BASE
+
+hdiutil convert installer_osx/template.dmg -format UDSP -o installer_osx/template
+hdiutil attach installer_osx/template.sparseimage
+
+cp -R dist/dtella.app/ /Volumes/Dtella/Dtella.app
+cp docs/readme.txt /Volumes/Dtella/
+cp docs/changelog.txt /Volumes/Dtella/
+cp docs/gpl.txt /Volumes/Dtella/
+
+diskutil rename /Volumes/Dtella/ $FILE_BASE
+hdiutil eject /Volumes/$FILE_BASE
+
+hdiutil convert installer_osx/template.sparseimage -format UDBZ -o $FILE_BASE.dmg
+
+rm installer_osx/template.sparseimage
